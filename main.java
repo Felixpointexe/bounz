199d7458c5ccd1653d51c3cbd9d952b024036c02
Feljx <76926119+Felixpointexe@users.noreply.github.com>
2021-03-12 23:50:59 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,155 +1,193 @@
-int x, y;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-float a = 0;
-float bx, by;
-float bvx,bvy;
-int br;
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
 
-int selectable;
+public class sketch_210312c extends PApplet {
 
-int lvl = 0;
+
+int lvl = 1;
 
 ArrayList<Rectangle> Grectangles = new ArrayList<Rectangle>();
 ArrayList<Rectangle> Rrectangles = new ArrayList<Rectangle>();
 
+ArrayList<level> levels = new ArrayList<level>();
 
 
 
-void setup() {
 
-  size(400, 800);
+
+
+public void setup() {
+
+  
   background(255);
 
-  reset();
 
 
-  Grectangles.add(new Rectangle(width/2, height/2, 80, 100));
-  Grectangles.add(new Rectangle(0, 0,150, 30));
-  
-  Rrectangles.add(new Rectangle(width/2-80, height/7, 80, 100));
+  levels.add(new level(new float[][]{{1, 0.5f, 0.5f, 0.2f, 0.25f}, {0, 0, 0, 0.8f, 0.1f}}));
+  levels.add(new level(new float[][]{{0, 0, 0.5f, 0.9f, 0.05f}, {0, 0.2f, 0.4f, 0.8f, 0.05f},{0, 0, 0.3f, 0.9f, 0.05f}, {0, 0.2f, 0.2f, 0.8f, 0.05f}}));
+  levels.add(new level(new float[][]{{1, 0.5f, 0.5f, 0.2f, 0.25f}, {0, 0, 0, 0.8f, 0.1f}}));
 }
 
-void draw() {
-  //background(255);
+public void draw() {
+  if (lvl< levels.size()) {
 
+    level level = levels.get(lvl-1);
+    level.run();
+  }
+}
+class level {
 
+  int x, y;
 
+  float a = 0;
+  float bx, by;
+  float bvx, bvy;
+  int br;
 
-  if (mousePressed && selectable < 2) {
-    selectable = 1;
-    background(255);
-    stroke(0, 0, 0, 60);
-    line(x, y, mouseX, mouseY);
+  int selectable;
 
-    //println((atan2(y-mouseY, mouseX-x)+ TWO_PI ) % TWO_PI);
-    a = (atan2(y-mouseY, mouseX-x)+ TWO_PI ) % TWO_PI;
-  } else {
-    if (selectable == 1) {
-      selectable = 2;
-      bvx = cos(a) * 6;
-      bvy = -1*sin(a) * 6;
+  ArrayList<Rectangle> Grectangles = new ArrayList<Rectangle>();
+  ArrayList<Rectangle> Rrectangles = new ArrayList<Rectangle>();
+
+  level(float[][] data) {
+    for (int i = 0; i < data.length; i++) {
+      if (data[i][0] == 0) {
+        Grectangles.add(new Rectangle(width*data[i][1], height*data[i][2], width*data[i][3], height*data[i][4]));
+      }
+      if (data[i][0] == 1) {
+        Rrectangles.add(new Rectangle(width*data[i][1], height*data[i][2], width*data[i][3], height*data[i][4]));
+      }
     }
-    x = width/2;
-    y = int(height*0.9);
-  }
 
-  fill(0, 255, 0, 60);
-  noStroke();
-  ellipse(bx, by, 2 * br, 2 * br);
-
-  bx += bvx;
-  by += bvy;
-
-  if (bx > width - br || bx < br) {
-    bvx *= -1;
-  }
-  if (by > height - br ) {
-    bvy *= -1;
-  }
-  if (by < br ) {
-    lvl++;
     reset();
   }
 
-
-  for (int i = 0; i < Grectangles.size(); i++) {
-
-    Rectangle rectangle = Grectangles.get(i);
-
-    //check X movment bounce
-    if (bx + 2*br + bvx > rectangle.x && 
-      bx + bvx < rectangle.x + rectangle.rectWidth && 
-      by + 2*br > rectangle.y && 
-      by < rectangle.y + rectangle.rectHeight) {
-
-      bvx *= -1;
-      
-    }
-
-    //check Y movement bounce
-    if (bx + 2*br > rectangle.x && 
-      bx < rectangle.x + rectangle.rectWidth && 
-      by + 2*br + bvy > rectangle.y && 
-      by + bvy < rectangle.y + rectangle.rectHeight) {
+  public void run() {
 
 
-      bvy *= -1;
+    if (mousePressed && selectable < 2) {
+      selectable = 1;
+      background(255);
+      stroke(0, 0, 0, 60);
+      line(x, y, mouseX, mouseY);
+
+      //println((atan2(y-mouseY, mouseX-x)+ TWO_PI ) % TWO_PI);
+      a = (atan2(y-mouseY, mouseX-x)+ TWO_PI ) % TWO_PI;
+    } else {
+      if (selectable == 1) {
+        selectable = 2;
+        bvx = cos(a) * 6;
+        bvy = -1*sin(a) * 6;
+      }
+      x = width/2;
+      y = PApplet.parseInt(height*0.9f);
     }
 
     fill(0, 255, 0, 60);
     noStroke();
-    rect(rectangle.x, rectangle.y, rectangle.rectWidth, rectangle.rectHeight,30);
-  }
-  
-  for (int i = 0; i < Rrectangles.size(); i++) {
+    ellipse(bx, by, 2 * br, 2 * br);
 
-    Rectangle rectangle = Rrectangles.get(i);
+    bx += bvx;
+    by += bvy;
 
-    //check X movment bounce
-    if (bx + 2*br + bvx > rectangle.x && 
-      bx + bvx < rectangle.x + rectangle.rectWidth && 
-      by + 2*br > rectangle.y && 
-      by < rectangle.y + rectangle.rectHeight) {
-
-      reset();
-      
+    if (bx > width - br || bx < br) {
+      bvx *= -1;
     }
-
-    //check Y movement bounce
-    if (bx + 2*br > rectangle.x && 
-      bx < rectangle.x + rectangle.rectWidth && 
-      by + 2*br + bvy > rectangle.y && 
-      by + bvy < rectangle.y + rectangle.rectHeight) {
-
-
+    if (by > height - br ) {
+      bvy *= -1;
+    }
+    if (by < br ) {
+      lvl++;
       reset();
     }
 
-    fill(255, 0, 0, 60);
+
+    for (int i = 0; i < Grectangles.size(); i++) {
+
+      Rectangle rectangle = Grectangles.get(i);
+
+      //check X movment bounce
+      if (bx + 2*br + bvx > rectangle.x && 
+        bx + bvx < rectangle.x + rectangle.rectWidth && 
+        by + 2*br > rectangle.y && 
+        by < rectangle.y + rectangle.rectHeight) {
+
+        bvx *= -1;
+      }
+
+      //check Y movement bounce
+      if (bx + 2*br > rectangle.x && 
+        bx < rectangle.x + rectangle.rectWidth && 
+        by + 2*br + bvy > rectangle.y && 
+        by + bvy < rectangle.y + rectangle.rectHeight) {
+
+
+        bvy *= -1;
+      }
+
+      fill(0, 255, 0, 60);
+      noStroke();
+      rect(rectangle.x, rectangle.y, rectangle.rectWidth, rectangle.rectHeight, 30);
+    }
+
+    for (int i = 0; i < Rrectangles.size(); i++) {
+
+      Rectangle rectangle = Rrectangles.get(i);
+
+      //check X movment bounce
+      if (bx + 2*br + bvx > rectangle.x && 
+        bx + bvx < rectangle.x + rectangle.rectWidth && 
+        by + 2*br > rectangle.y && 
+        by < rectangle.y + rectangle.rectHeight) {
+
+        reset();
+      }
+
+      //check Y movement bounce
+      if (bx + 2*br > rectangle.x && 
+        bx < rectangle.x + rectangle.rectWidth && 
+        by + 2*br + bvy > rectangle.y && 
+        by + bvy < rectangle.y + rectangle.rectHeight) {
+
+
+        reset();
+      }
+
+      fill(255, 0, 0, 60);
+      noStroke();
+      rect(rectangle.x, rectangle.y, rectangle.rectWidth, rectangle.rectHeight, 30);
+    }
+    
+
+    fill(210, 52, 235, 60);
+    textSize(width/10);
     noStroke();
-    rect(rectangle.x, rectangle.y, rectangle.rectWidth, rectangle.rectHeight,30);
+    text(lvl, width*0.1f, height*0.1f);
   }
 
-  fill(210, 52, 235 ,60);
-  textSize(width/10);
-  text(lvl, width*0.1, height*0.1);
+
+  public void reset() {
+    background(255);
+    bx = width/2;
+    by = PApplet.parseInt(height*0.9f);
+    bvx = 0;
+    bvy = 0;
+
+    br = width/40;
+    selectable = 0;
+  }
 }
-
-
-void reset() {
-  background(255);
-  bx = width/2;
-  by = int(height*0.9);
-  bvx = 0;
-  bvy = 0;
-  
-  br = width/40;
-  selectable = 0;
-}
-
-
-
-
 class Rectangle {
   float x;
   float y;
@@ -161,5 +199,15 @@ class Rectangle {
     this.y = y;
     this.rectWidth = rectWidth;
     this.rectHeight = rectHeight;
+  }
+}
+  public void settings() {  size(400, 800); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "sketch_210312c" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
